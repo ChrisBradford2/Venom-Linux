f6b9cba26f65ca8f006afda36f5e2ee096bc703b
Restyled.io <commits@restyled.io>
2022-10-04 14:59:11 +0000

    Restyled by prettier

--- sources/js/application_list.js
+++ sources/js/application_list.js
@@ -20,21 +20,29 @@ class Application {
 }
 
 const figma = new Application(
-    "figma", "Figma",
-    "https://www.figma.com/file/SZWyC97gtK3khQtp0NgFEE/OS-project?node-id=0%3A1",
-    true);
-const github = new Application("github", "Github",
-                               "https://github.com/ChrisBradford2/Venom-Linux");
-const firefox =
-    new Application("firefox", "Firefox", "https://fonts.googleapis.com");
+  "figma",
+  "Figma",
+  "https://www.figma.com/file/SZWyC97gtK3khQtp0NgFEE/OS-project?node-id=0%3A1",
+  true
+);
+const github = new Application(
+  "github",
+  "Github",
+  "https://github.com/ChrisBradford2/Venom-Linux"
+);
+const firefox = new Application(
+  "firefox",
+  "Firefox",
+  "https://fonts.googleapis.com"
+);
 
 // Add the application in the array :
-let applicationList = [ github, figma, firefox ];
+let applicationList = [github, figma, firefox];
 
 function formdata() {
   let newFileName = document.getElementById("new-file-name").value;
   console.log(newFileName);
-  const new_file = new Application("file", newFileName, "#")
+  const new_file = new Application("file", newFileName, "#");
   console.log(new_file);
   applicationList.push(new_file);
   console.log(applicationList);
@@ -51,8 +59,7 @@ function formdata() {
   document.getElementById("application-list").innerHTML += `
   <div class="desktop--app-list--icon">
       <a href="${obj.link}">
-          <img src="assets/icons/${obj.icon}.svg" alt="${
-      obj.icon} text file application" />
+          <img src="assets/icons/${obj.icon}.svg" alt="${obj.icon} text file application" />
           <p>${obj.name}.txt</p>
       </a>
   </div>
@@ -61,7 +68,7 @@ function formdata() {
   modal.style.display = "none";
 }
 
-applicationList.forEach(function(application) {
+applicationList.forEach(function (application) {
   // Storing data:
   const applicationJSON = JSON.stringify(application);
   localStorage.setItem("applicationItem", applicationJSON);
@@ -82,8 +89,7 @@ applicationList.forEach(function(application) {
   document.getElementById("application-list").innerHTML += `
     <div class="desktop--app-list--icon">
         <a href="${obj.link}" ${blank}>
-            <img src="assets/icons/${obj.icon}.svg" alt="${
-      obj.icon} application" />
+            <img src="assets/icons/${obj.icon}.svg" alt="${obj.icon} application" />
             <p>${obj.name}</p>
         </a>
     </div>
