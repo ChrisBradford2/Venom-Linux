68ed5d3fe7417b73fbb8e1a6f37bd60d5ac8a94a
Nicolas Barbarisi <nico.bar2012@gmail.com>
2022-11-07 11:48:31 +0100

    #2 : Include Tictactoe in modal.

--- sources/js/tictactoe.js
+++ sources/js/tictactoe.js
@@ -1,3 +1,33 @@
+// Modal
+
+const modalTicTacToe = document.getElementById('modal-tictactoe')
+
+// Get the button that opens the modal
+const btnTicTacToe = document.getElementById('tictactoe-application')
+console.log(btnTicTacToe)
+
+// Get the <span> element that closes the modal
+const spanTicTacToe = document.getElementById('close-tictactoe')
+console.log(spanTicTacToe)
+
+// When the user clicks the button, open the modal
+btnTicTacToe.onclick = function () {
+  modalTicTacToe.style.display = 'block'
+  console.log('Get the modal')
+}
+
+// When the user clicks on <span> (x), close the modal
+spanTicTacToe.onclick = function () {
+  modalTicTacToe.style.display = 'none'
+}
+
+// When the user clicks anywhere outside of the modal, close it.
+window.onclick = function (event) {
+  if (event.target === modalTicTacToe) {
+    modalTicTacToe.style.display = 'none'
+  }
+}
+
 /* -----------------------------------------------------------------------------
 * Global Variables
 * -----------------------------------------------------------------------------
@@ -14,7 +44,6 @@ let counterForDraw = 1
 
 const btnStartGame = document.getElementById('startGame')
 const msgError = document.getElementById('error')
-const closeGame = document.getElementById('closeGame')
 const btnNextGame = document.getElementById('nextGame')
 const btnResetGame = document.getElementById('resetGame')
 
@@ -28,7 +57,7 @@ btnStartGame.addEventListener('click', function (event) {
   player1 = document.getElementById('namePlayer1').value
   player2 = document.getElementById('namePlayer2').value
 
-  if (player1 !== '' && player2 !== '') {
+  if ('' !== player1 && '' !== player2) {
     if (player1 !== player2) {
       resetBoard()
       turn = player1
@@ -72,51 +101,6 @@ function resetBoard () {
 * -----------------------------------------------------------------------------
 */
 
-closeGame.addEventListener('click', function () {
-  window.close()
-})
-
-/* -----------------------------------------------------------------------------
-* initializeCellAndPlay : Initalize game board, play, and check if there is a winner
-* -----------------------------------------------------------------------------
-*/
-
-function initializeCellAndPlay () {
-  btnResetGame.addEventListener('click', resetBoard)
-  btnNextGame.addEventListener('click', nextGame)
-
-  const cells = document.getElementsByTagName('td')
-
-  for (let i = 0; i < cells.length; i++) {
-    cells[i].addEventListener('click', function (event) {
-      if (winner === '' && cells[i].className === 'notClicked') {
-        play(cells[i])
-        checkWin(cells)
-        checkDraw(cells)
-      }
-    })
-  }
-}
-
-/* -----------------------------------------------------------------------------
-* play : put symbol in played cell, and change classname
-* -----------------------------------------------------------------------------
-*/
-
-function play (playedCell) {
-  if (playedCell.className === 'notClicked' && winner === '') {
-    if (turn === player1) {
-      playedCell.className = 'clicked'
-      playedCell.innerText = 'X'
-      turn = player2
-    } else {
-      playedCell.className = 'clicked'
-      playedCell.innerText = 'O'
-      turn = player1
-    }
-  }
-}
-
 /* -----------------------------------------------------------------------------
 * checkWin : Check if we are a winner
 *     <table> |---|---|---|
@@ -151,12 +135,12 @@ const checkWin = (allCells) => {
     if (allCells[a].innerText && allCells[a].innerText === allCells[b].innerText && allCells[b].innerText === allCells[c].innerText) {
       winner = allCells[a].innerText
 
-      if (winner === 'X') {
+      if ('X' === winner) {
         winner = player1
-      } else if (winner === 'O') {
+      } else if ('O' === winner) {
         winner = player2
       }
-      btnNextGame.style.visibility = 'visible'
+      btnNextGame.style.display = 'block'
     }
   }
   updateGameInformations({ winner, turn })
@@ -169,39 +153,82 @@ const checkWin = (allCells) => {
 
 function checkDraw (allPlace) {
   for (let i = 0; i < allPlace.length; i++) {
-    if (allPlace[i].className === 'clicked') {
+    if ('clicked' === allPlace[i].className) {
       counterForDraw++
       break
     }
   }
-  if (counterForDraw === 10 && winner === '') {
+  if (10 === counterForDraw && '' === winner) {
     winner = 'none'
     turn = null
     updateGameInformations({ winner, turn })
   }
 }
+
+/* -----------------------------------------------------------------------------
+* initializeCellAndPlay : Initalize game board, play, and check if there is a winner
+* -----------------------------------------------------------------------------
+*/
+
+function initializeCellAndPlay () {
+  document.getElementById('checkbox-vibration-toggle').style.backgroundColor = 'red'
+  btnResetGame.addEventListener('click', resetBoard)
+  btnNextGame.addEventListener('click', nextGame)
+
+  const cells = document.getElementsByTagName('td')
+
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].addEventListener('click', function (event) {
+      if ('' === winner && 'notClicked' === cells[i].className) {
+        play(cells[i])
+        checkWin(cells)
+        checkDraw(cells)
+      }
+    })
+  }
+}
+
+/* -----------------------------------------------------------------------------
+* play : put symbol in played cell, and change classname
+* -----------------------------------------------------------------------------
+*/
+
+function play (playedCell) {
+  if ('notClicked' === playedCell.className && '' === winner) {
+    if (turn === player1) {
+      playedCell.className = 'clicked'
+      playedCell.innerText = 'X'
+      turn = player2
+    } else {
+      playedCell.className = 'clicked'
+      playedCell.innerText = 'O'
+      turn = player1
+    }
+  }
+}
+
 /* -----------------------------------------------------------------------------
 * updateGameInformations : Update game information like turn, score and display state of game
 * -----------------------------------------------------------------------------
 */
 function updateGameInformations ({ winner, turn }) {
-  const h2 = document.getElementsByTagName('h2')[0]
+  const h2 = document.getElementById('turn')
 
   const player1Score = document.getElementById('playerX1')
   const player2Score = document.getElementById('playerO2')
 
-  if (winner === '') {
+  if ('' === winner) {
     h2.innerText = 'It\'s your turn ' + turn
   } else {
     if (winner === player1) {
       h2.innerText = 'Winner is ' + player1 + ' !'
       score.p1++
-      btnResetGame.style.visibility = 'hidden'
+      btnResetGame.style.display = 'none'
     } else if (winner === player2) {
       h2.innerText = 'Winner is ' + player2 + ' !'
       score.p2++
-      btnResetGame.style.visibility = 'hidden'
-    } else if (winner === 'none') {
+      btnResetGame.style.display = 'none'
+    } else if ('none' === winner) {
       h2.innerText = 'It\'s a tie'
     }
   }
@@ -214,8 +241,8 @@ function updateGameInformations ({ winner, turn }) {
 * -----------------------------------------------------------------------------
 */
 function nextGame () {
-  btnNextGame.style.visibility = 'hidden'
-  btnResetGame.style.visibility = 'visible'
+  btnNextGame.style.display = 'none'
+  btnResetGame.style.display = 'block'
   resetBoard()
 }
 
@@ -226,7 +253,7 @@ function nextGame () {
 
 function hideForm () {
   const playerForm = document.getElementById('playerForm')
-  playerForm.style.visibility = 'hidden'
+  playerForm.style.display = 'none'
 }
 
 /* -----------------------------------------------------------------------------
@@ -237,6 +264,6 @@ function hideForm () {
 function showGame () {
   const gameBoard = document.getElementById('gameBoard')
   const infoBlock = document.getElementById('infoBlock')
-  gameBoard.style.visibility = 'visible'
-  infoBlock.style.visibility = 'visible'
+  gameBoard.style.display = 'block'
+  infoBlock.style.display = 'block'
 }
