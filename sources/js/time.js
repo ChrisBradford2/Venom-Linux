478bd6f4ce8ea8c8c4c5463ffbd3869407ca1edd
Nicolas Barbarisi <nico.bar2012@gmail.com>
2022-11-07 14:31:16 +0100

    #11 : Add stopwtach

--- sources/js/time.js
+++ sources/js/time.js
@@ -20,3 +20,54 @@ btnTime.onclick = function () {
 spanTime.onclick = function () {
   modalTime.style.display = 'none'
 }
+
+window.onload = function () {
+  let seconds = 0
+  let tens = 0
+  const appendTens = document.getElementById('tens')
+  const appendSeconds = document.getElementById('secs')
+  const buttonStart = document.getElementById('button-start')
+  const buttonStop = document.getElementById('button-stop')
+  const buttonReset = document.getElementById('button-reset')
+  let Interval
+
+  buttonStart.onclick = function () {
+    clearInterval(Interval)
+    Interval = setInterval(startTimer, 10)
+  }
+
+  buttonStop.onclick = function () {
+    clearInterval(Interval)
+  }
+
+  buttonReset.onclick = function () {
+    clearInterval(Interval)
+    tens = '00'
+    seconds = '00'
+    appendTens.innerHTML = tens
+    appendSeconds.innerHTML = seconds
+  }
+
+  function startTimer () {
+    tens++
+
+    if (9 >= tens) {
+      appendTens.innerHTML = '0' + tens
+    }
+
+    if (9 < tens) {
+      appendTens.innerHTML = tens
+    }
+
+    if (99 < tens) {
+      seconds++
+      appendSeconds.innerHTML = '0' + seconds
+      tens = 0
+      appendTens.innerHTML = '0' + 0
+    }
+
+    if (9 < seconds) {
+      appendSeconds.innerHTML = seconds
+    }
+  }
+}
